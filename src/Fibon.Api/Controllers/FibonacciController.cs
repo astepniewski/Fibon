81249e6749a780bf198f349ec58e4111bdb3882e
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2017-06-24 10:26:23 +0200

    Added controllers

---
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public async Task<IActionResult> Get(int number)
+        {
+            await Task.CompletedTask;
+            return Content(number.ToString());
+        }
+
+        [HttpPost("{number}")]
+        public async Task<IActionResult>Post(int number)
+        {
+            await Task.CompletedTask;
+            return Accepted($"fibonnacci/{number}", null);
+        }
+    }
+}
