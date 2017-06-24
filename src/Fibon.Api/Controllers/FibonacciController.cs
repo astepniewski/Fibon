6b68ceb77a84aa7ca1c9e8b9b756e762efc747d8
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2017-06-24 10:53:25 +0200

    Configuret services.

--- src/Fibon.Api/Controllers/FibonacciController.cs
+++ src/Fibon.Api/Controllers/FibonacciController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
 using System.Threading.Tasks;
 
 namespace Fibon.Api.Controllers
@@ -6,6 +7,13 @@ namespace Fibon.Api.Controllers
     [Route("[controller]")]
     public class FibonacciController : Controller
     {
+        private readonly IBusClient _busclient;
+
+        public FibonacciController(IBusClient busClient)
+        {
+            _busclient = busClient;
+        }
+
         [HttpGet("{number}")]
         public async Task<IActionResult> Get(int number)
         {
@@ -14,7 +22,7 @@ namespace Fibon.Api.Controllers
         }
 
         [HttpPost("{number}")]
-        public async Task<IActionResult>Post(int number)
+        public async Task<IActionResult> Post(int number)
         {
             await Task.CompletedTask;
             return Accepted($"fibonnacci/{number}", null);
